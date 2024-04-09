69c21313aefff9feafc15e835a1effd4f5335a75
cox0902-mini <cox0902@163.com>
2024-04-09 18:37:24 +0800

    +

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ import numpy as np
 from datetime import timedelta
 
 import torch
+import torch.nn.functional as F
 from torch import nn
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
