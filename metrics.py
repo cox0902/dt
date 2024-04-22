cdf5d5490fdd957c9ac84880968134b6b24c2f7a
cox0902-mini <cox0902@163.com>
2024-04-22 21:52:48 +0800

    +

--- metrics.py
+++ metrics.py
@@ -104,5 +104,5 @@ class BinaryMetrics(Metrics):
         print(tabulate([["T", f"TP {m[0][0]}", f"FN {m[0][1]}"], ["F", f"FP {m[1][0]}", f"TN {m[1][1]}"]], 
                        headers=["", "P", "N"], tablefmt="psql"))
         print(f'* POS * Pre {TP / (TP + FP):.5f} Rec {TP / (TP + FN):.5f} F-1 {2 * TP / (2 * TP + FP + FN):.5f}')
-        print(f'* NEG * Pre {TN / (TN + FN):.5f} Rec {TN / (TN + FP):.5f} F-1 {2 * TN / (2 * TN + FP + FN):.5f}')
+        print(f'* NEG * Pre {TN / (TN + FN):.5f} Rec {TN / (TN + FP):.5f} F-1 {2 * TN / (2 * TN + FP + FN):.5f}\n')
         return score
