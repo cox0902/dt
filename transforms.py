070deb9dec0d8c2edde674670bbf769cc00e7c1e
cox0902-mini <cox0902@163.com>
2024-05-03 11:24:47 +0800

    *

--- transforms.py
+++ transforms.py
@@ -5,6 +5,59 @@ import torchvision.transforms.v2.functional as F
 from torchvision import tv_tensors
 
 
+# class TrivialAugmentGui(T.TrivialAugmentWide):
+
+#     _AUGMENTATION_SPACE = {
+#         "Identity": (lambda num_bins, height, width: None, False),
+
+#         # "ShearX": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
+#         # "ShearY": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
+#         # "TranslateX": (lambda num_bins, height, width: torch.linspace(0.0, 32.0, num_bins), True),
+#         # "TranslateY": (lambda num_bins, height, width: torch.linspace(0.0, 32.0, num_bins), True),
+#         # "Rotate": (lambda num_bins, height, width: torch.linspace(0.0, 135.0, num_bins), True),
+        
+#         "Brightness": (lambda num_bins, height, width: torch.linspace(-0.5, 0.5, num_bins), True),
+#         "Color": (lambda num_bins, height, width: torch.linspace(-0.99, 0.99, num_bins), True),
+#         "Contrast": (lambda num_bins, height, width: torch.linspace(-0.5, 0.99, num_bins), True),
+#         "Sharpness": (lambda num_bins, height, width: torch.linspace(-0.99, 0.99, num_bins), True),
+#         "Posterize": (
+#             lambda num_bins, height, width: (8 - (torch.arange(num_bins) / ((num_bins - 1) / 6))).round().int(),
+#             False,
+#         ),
+#         "Solarize": (lambda num_bins, height, width: torch.linspace(1.0, 0.0, num_bins), False),
+#         "AutoContrast": (lambda num_bins, height, width: None, False),
+#         # "Equalize": (lambda num_bins, height, width: None, False),
+#     }
+
+#     def __init__(
+#         self,
+#         num_magnitude_bins: int = 31,
+#         interpolation = F.InterpolationMode.NEAREST,
+#         fill = None,
+#     ):
+#         super().__init__(interpolation=interpolation, fill=fill)
+#         self.num_magnitude_bins = num_magnitude_bins
+
+#     def forward(self, *inputs):
+#         flat_inputs_with_spec, image_or_video = self._flatten_and_extract_image_or_video(inputs)
+#         height, width = F.get_size(image_or_video)
+
+#         transform_id, (magnitudes_fn, signed) = self._get_random_item(self._AUGMENTATION_SPACE)
+
+#         magnitudes = magnitudes_fn(self.num_magnitude_bins, height, width)
+#         if magnitudes is not None:
+#             magnitude = float(magnitudes[int(torch.randint(self.num_magnitude_bins, ()))])
+#             # if signed and torch.rand(()) <= 0.5:
+#             #     magnitude *= -1
+#         else:
+#             magnitude = 0.0
+
+#         image_or_video = self._apply_image_or_video_transform(
+#             image_or_video, transform_id, magnitude, interpolation=self.interpolation, fill=self._fill
+#         )
+#         return self._unflatten_and_insert_image_or_video(flat_inputs_with_spec, image_or_video)
+
+
 class TrivialAugmentGui(T.TrivialAugmentWide):
 
     _AUGMENTATION_SPACE = {
@@ -15,11 +68,11 @@ class TrivialAugmentGui(T.TrivialAugmentWide):
         # "TranslateX": (lambda num_bins, height, width: torch.linspace(0.0, 32.0, num_bins), True),
         # "TranslateY": (lambda num_bins, height, width: torch.linspace(0.0, 32.0, num_bins), True),
         # "Rotate": (lambda num_bins, height, width: torch.linspace(0.0, 135.0, num_bins), True),
-        
-        "Brightness": (lambda num_bins, height, width: torch.linspace(-0.5, 0.5, num_bins), True),
-        "Color": (lambda num_bins, height, width: torch.linspace(-0.99, 0.99, num_bins), True),
-        "Contrast": (lambda num_bins, height, width: torch.linspace(-0.5, 0.99, num_bins), True),
-        "Sharpness": (lambda num_bins, height, width: torch.linspace(-0.99, 0.99, num_bins), True),
+
+        # "Brightness": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
+        "Color": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
+        "Contrast": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
+        "Sharpness": (lambda num_bins, height, width: torch.linspace(0.0, 0.99, num_bins), True),
         "Posterize": (
             lambda num_bins, height, width: (8 - (torch.arange(num_bins) / ((num_bins - 1) / 6))).round().int(),
             False,
@@ -28,34 +81,6 @@ class TrivialAugmentGui(T.TrivialAugmentWide):
         "AutoContrast": (lambda num_bins, height, width: None, False),
         # "Equalize": (lambda num_bins, height, width: None, False),
     }
-
-    def __init__(
-        self,
-        num_magnitude_bins: int = 31,
-        interpolation = F.InterpolationMode.NEAREST,
-        fill = None,
-    ):
-        super().__init__(interpolation=interpolation, fill=fill)
-        self.num_magnitude_bins = num_magnitude_bins
-
-    def forward(self, *inputs):
-        flat_inputs_with_spec, image_or_video = self._flatten_and_extract_image_or_video(inputs)
-        height, width = F.get_size(image_or_video)
-
-        transform_id, (magnitudes_fn, signed) = self._get_random_item(self._AUGMENTATION_SPACE)
-
-        magnitudes = magnitudes_fn(self.num_magnitude_bins, height, width)
-        if magnitudes is not None:
-            magnitude = float(magnitudes[int(torch.randint(self.num_magnitude_bins, ()))])
-            # if signed and torch.rand(()) <= 0.5:
-            #     magnitude *= -1
-        else:
-            magnitude = 0.0
-
-        image_or_video = self._apply_image_or_video_transform(
-            image_or_video, transform_id, magnitude, interpolation=self.interpolation, fill=self._fill
-        )
-        return self._unflatten_and_insert_image_or_video(flat_inputs_with_spec, image_or_video)
 
 
 class GuiVisPresetTrain:
