16f2e48c919f57bb6453a7ec136cc7f28413f0bd
cox0902-mini <cox0902@163.com>
2024-05-03 19:42:19 +0800

    -

--- datasets.py
+++ datasets.py
@@ -9,42 +9,41 @@ from torch.utils.data import Dataset
 
 class GuiVisDataset(Dataset):
 
-    def __init__(self, data_path: str, data_name: str, 
-                 mask_mode: Literal["binary", "gray"] = "binary", 
-                 transform = None):
+    def __init__(self, data_path: str, data_name: str, transform = None, 
+                 label_smooth: bool = True):
         self.h = h5py.File(data_path + data_name + ".hdf5", "r")
         self.images = self.h["images"]
         self.ricoid = self.h["ricoid"]
         self.masks = self.h["masks"]
-        self.mask_mode = mask_mode
         self.rects = self.h["rects"]
         self.labels = self.h["labels"]
         self.transform = transform
         self.mode = None
         self.extras = 0
         self.k = 10
+        self.label_smooth = label_smooth
 
     def summary(self):
-        print(f"Mask Grayscaled: {len(np.unique(self.masks[0])) != 2}")
-        print(f"Binary Weights : {1. / np.bincount(self.labels[:, 2])}")
+        print(f"Dataset Size: {len(self)}")
+        print(f" Labels Size: {np.bincount(self.labels[:, 2])}")
 
     def __getitem__(self, i: int):
-        if i >= len(self.labels):
+        if i >= len(self.labels) or -self.extras <= i < 0:
             return self.__getoovitem()
         img_idx = self.labels[i, 0]
         label = self.labels[i, 2]
         img = torch.from_numpy(self.images[img_idx])
-        if self.mask_mode == "binary":
-            img_mask = torch.FloatTensor(self.masks[i] == 255)
-        else:
-            img_mask = torch.FloatTensor(self.masks[i] / 255.)
+        img_mask = torch.FloatTensor(self.masks[i] / 255.)
         img_rect = torch.FloatTensor(self.rects[i])
         # img = torch.FloatTensor(self.images[img_idx] / 255.)
         # img_mask = torch.FloatTensor(self.masks[i] / 255.)
         if self.transform is not None:
             img, img_mask, _ = self.transform(img, img_mask, img_rect)
         img = torch.cat([img, img_mask], dim=0)
-        label = torch.FloatTensor([label])
+        if self.label_smooth:
+            label = torch.FloatTensor([np.clip(label, 0.1, 0.9)])
+        else:
+            label = torch.FloatTensor([label])
         return {
             "image": img, 
             "target": label
@@ -53,15 +52,18 @@ class GuiVisDataset(Dataset):
     def __getoovitem(self):
         img_index = np.random.randint(len(self.images))
         img = torch.from_numpy(self.images[img_index])
-        
+        # print("sampled : ", self.labels[np.random.choice(np.where(self.labels[:, 0] == img_index)[0])])
+
+        msk_indices_pos = np.where(self.labels[:, 0] == img_index)[0]
+        img_masks_pos = self.masks[msk_indices_pos] / 255.
+
         if self.mode == "rnd":
             msk_index = np.random.choice(np.where(self.labels[:, 0] != img_index)[0])
+            # print("randomed: ", self.labels[msk_index])
             img_mask = torch.FloatTensor(self.masks[msk_index] / 255.)
             img_rect = torch.FloatTensor(self.rects[msk_index])
         else:  # "neg"
-            msk_indices_pos = np.where(self.labels[:, 0] != img_index)[0]
-            img_masks_pos = self.masks[msk_indices_pos] / 255.
-            img_mask_pos = np.sum(img_masks_pos, axis=0)
+            img_mask_pos = np.sum(img_masks_pos, axis=0) / len(msk_indices_pos)
 
             msk_indices_neg = np.random.choice(
                 np.where(self.labels[:, 0] != img_index)[0], 
@@ -71,18 +73,29 @@ class GuiVisDataset(Dataset):
 
             img_masks_weights = img_mask_pos * img_masks_neg
             img_masks_weights = img_masks_weights.reshape(self.k, -1)
-            img_masks_weights = np.average(img_masks_weights, axis=1)
+            img_masks_weights = np.sum(img_masks_weights, axis=1) / np.sum(img_mask_pos)
 
-            msk_index_neg = np.random.choice(np.where(img_masks_weights == img_masks_weights.min())[0])
+            p = 1 - img_masks_weights
+            p = p / np.sum(p)
+            msk_index_neg = np.random.choice(len(img_masks_weights), p=p)
             msk_index_neg = msk_indices_neg[msk_index_neg]
 
             img_mask = torch.FloatTensor(self.masks[msk_index_neg] / 255.)
             img_rect = torch.FloatTensor(self.rects[msk_index_neg])
+
+        if self.label_smooth:
+            mask_i = np.logical_and(img_masks_pos, img_mask)
+            mask_i = np.sum(mask_i, axis=(1, 2, 3))
+            mask_u = np.logical_or(img_masks_pos, img_mask)
+            mask_u = np.sum(mask_u, axis=(1, 2, 3))
+            mask_iou = mask_i / mask_u
+            label = torch.FloatTensor([np.max(mask_iou)])
+        else:
+            label = torch.FloatTensor([0])
         
         if self.transform is not None:
             img, img_mask, _ = self.transform(img, img_mask, img_rect)
         img = torch.cat([img, img_mask], dim=0)
-        label = torch.FloatTensor([0])
         return { 
             "image": img, 
             "target": label
--- helpers.py
+++ helpers.py
@@ -41,7 +41,7 @@ def plot(imgs, titles=None, row_title=None, figshow_kwargs = {}, imshow_kwargs =
             if boxes is not None:
                 img = draw_bounding_boxes(img, boxes, colors="yellow", width=3)
             if masks is not None:
-                img = draw_segmentation_masks(img, masks.to(torch.bool), colors=["green"] * masks.shape[0], alpha=.65)
+                img = draw_segmentation_masks(img, masks.to(torch.bool), colors=[(255, 0, 255)] * masks.shape[0], alpha=.65)
 
             ax = axs[row_idx, col_idx]
             ax.imshow(img.permute(1, 2, 0).numpy(), **imshow_kwargs)
