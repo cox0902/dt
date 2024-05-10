987da64c4dae4a1f2edb8207ecad411d8d763f8e
cox0902-mini <cox0902@163.com>
2024-05-10 14:19:42 +0800

    *

--- datasets.py
+++ datasets.py
@@ -19,6 +19,7 @@ class GuiVisDataset(Dataset):
         self.labels = self.h["labels"]
         self.transform = transform
         self.mode = None
+        self.leaf_only = False
         self.extras = 0
         self.k = 10
         self.label_smooth = label_smooth
@@ -54,48 +55,72 @@ class GuiVisDataset(Dataset):
         img = torch.from_numpy(self.images[img_index])
         # print("sampled : ", self.labels[np.random.choice(np.where(self.labels[:, 0] == img_index)[0])])
 
-        msk_indices_pos = np.where(np.logical_and(
-            self.labels[:, 0] == img_index, self.labels[:, 4] == 1
-        ))[0]
+        if self.leaf_only:
+            msk_indices_pos = np.where(np.logical_and(
+                self.labels[:, 0] == img_index, self.labels[:, 4] == 1
+            ))[0]
+        else:
+            msk_indices_pos = np.where(self.labels[:, 0] == img_index)[0]
         img_masks_pos = self.masks[msk_indices_pos] / 255.
 
         if self.mode == "rnd":
-            msk_index = np.random.choice(np.where(np.logical_and(
-                self.labels[:, 0] != img_index, self.labels[:, 4] == 1
-            ))[0])
+            if self.leaf_only:
+                msk_index = np.random.choice(np.where(np.logical_and(
+                    self.labels[:, 0] != img_index, self.labels[:, 4] == 1
+                ))[0])
+            else:
+                msk_index = np.random.choice(np.where(self.labels[:, 0] != img_index)[0])
             # print("randomed: ", self.labels[msk_index])
             img_mask = torch.FloatTensor(self.masks[msk_index] / 255.)
             img_rect = torch.FloatTensor(self.rects[msk_index])
         else:  # "neg"
-            img_mask_pos = np.sum(img_masks_pos, axis=0) / len(msk_indices_pos)
+            # img_mask_pos = np.sum(img_masks_pos, axis=0) / len(msk_indices_pos)
 
-            msk_indices_neg = np.random.choice(
-                np.where(np.logical_and(
-                    self.labels[:, 0] != img_index, self.labels[:, 4] == 1
-                ))[0], (self.k, ), replace=False)
+            if self.leaf_only:
+                msk_indices_neg = np.random.choice(
+                    np.where(np.logical_and(
+                        self.labels[:, 0] != img_index, self.labels[:, 4] == 1
+                    ))[0], (self.k, ), replace=False)
+            else:
+                msk_indices_neg = np.random.choice(
+                    np.where(self.labels[:, 0] != img_index)[0], 
+                    (self.k, ), replace=False)
             msk_indices_neg = np.sort(msk_indices_neg)
             img_masks_neg = self.masks[msk_indices_neg] / 255.
 
-            img_masks_weights = img_mask_pos * img_masks_neg
-            img_masks_weights = img_masks_weights.reshape(self.k, -1)
-            img_masks_weights = np.sum(img_masks_weights, axis=1) / np.sum(img_mask_pos)
+            ious = np.zeros((img_masks_pos.shape[0], img_masks_neg.shape[0]), dtype=np.float32)
+            # print(ious.shape, img_masks_pos.shape, img_masks_neg.shape)
+            for i in range(img_masks_pos.shape[0]):
+                for j in range(img_masks_neg.shape[0]):
+                    inter = np.sum(np.logical_and(img_masks_pos[i, 0], img_masks_neg[j, 0]))
+                    union = np.sum(np.logical_or(img_masks_pos[i, 0], img_masks_neg[j, 0]))
+                    ious[i, j] = inter / union
 
-            p = 1 - img_masks_weights
-            p = p / np.sum(p)
-            assert not np.any(np.isnan(p)), img_index
-            msk_index_neg = np.random.choice(len(img_masks_weights), p=p)
+            m = np.max(ious, axis=0)
+            p = None if np.allclose(m, 1) else (1 - m) / np.sum(1 - m)
+            msk_index_neg = np.random.choice(len(img_masks_neg), p=p)
+            iou = m[msk_index_neg]
+
+            # img_masks_weights = img_mask_pos * img_masks_neg
+            # img_masks_weights = img_masks_weights.reshape(self.k, -1)
+            # img_masks_weights = np.sum(img_masks_weights, axis=1) / np.sum(img_mask_pos)
+
+            # p = 1 - img_masks_weights
+            # p = p / np.sum(p)
+            # assert not np.any(np.isnan(p)), img_index
+            # msk_index_neg = np.random.choice(len(img_masks_weights), p=p)
             msk_index_neg = msk_indices_neg[msk_index_neg]
 
             img_mask = torch.FloatTensor(self.masks[msk_index_neg] / 255.)
             img_rect = torch.FloatTensor(self.rects[msk_index_neg])
 
         if self.label_smooth:
-            mask_i = np.logical_and(img_masks_pos, img_mask.numpy())
-            mask_i = np.sum(mask_i, axis=(1, 2, 3))
-            mask_u = np.logical_or(img_masks_pos, img_mask.numpy())
-            mask_u = np.sum(mask_u, axis=(1, 2, 3))
-            mask_iou = mask_i / mask_u
-            label = torch.FloatTensor([np.max(mask_iou)])
+            # mask_i = np.logical_and(img_masks_pos, img_mask.numpy())
+            # mask_i = np.sum(mask_i, axis=(1, 2, 3))
+            # mask_u = np.logical_or(img_masks_pos, img_mask.numpy())
+            # mask_u = np.sum(mask_u, axis=(1, 2, 3))
+            # mask_iou = mask_i / mask_u
+            label = torch.FloatTensor([iou])
         else:
             label = torch.FloatTensor([0])
         
@@ -116,8 +141,9 @@ class GuiVisDataset(Dataset):
         labels_weights = 1.0 / np.bincount(labels)
         return labels_weights[labels]
     
-    def resample(self, mode: Literal["rnd", "neg"], k: int = 10):
+    def resample(self, mode: Literal["rnd", "neg"], k: int = 10, leaf_only: bool = False):
         self.mode = mode
+        self.leaf_only = leaf_only
         self.k = k
         bincount = np.bincount(self.labels[:, 2])
         self.extras = bincount[1] - bincount[0]
